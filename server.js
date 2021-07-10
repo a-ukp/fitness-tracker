4107251ae5fbef1e808a34c8c18d544bcecbe4dd
Anna <uyenkphung@gmail.com>
2021-07-10 10:18:55 -0500

    got pages to render

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const PORT = 3000;
+const PORT = 5000;
 const app = express();
 
 app.use(logger('dev'));
@@ -10,13 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/fitness', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+// mongoose.connect('mongodb://localhost/fitness', {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// });
 
-app.use(require('./routes/api'));
+// app.use(require('./routes/api'));
 app.use(require('./routes/view'));
 
 app.listen(PORT, () => {
